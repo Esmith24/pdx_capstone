6f400697296fe0414f87b99a05fa73bac2cee7fd
Elliott Smith <elliott.smith2437@gmail.com>
2021-12-23 14:05:25 -0800

    heroku

---
+++ static/carousel.js
@@ -0,0 +1,36 @@
+const track = document.querySelector('.carousel__track');
+    const slides = Array.from(track.children);
+    const nextButton = document.querySelector('.carousel__button--right');
+    const prevButton = document.querySelector('.carousel__button--left');
+    const dotsNav = document.querySelector('.carousel__nav');
+    const dots = Array.from(dotsNav.children);
+    const slideWidth = slides[0].getBoundingClientRect().width;
+
+    //arrange the slides next to one another
+    const setSlidePosition = (slide, index) => {
+        slide.style.left = slideWidth * index + 'px'
+    };
+    slides.forEach(setSlidePosition);
+
+    const moveToSlide = (track, currentSlide, targetSlide) => {
+        track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+        currentSlide.classList.remove('current-slide');
+        targetSlide.classList.add('current-slide');
+    }
+
+    //when I click, move slides to left
+    prevButton.addEventListener('click', e => {
+        const currentSlide = track.querySelector('.current-slide');
+        const prevSlide = currentSlide.previousElementSibling;
+
+        moveToSlide(track, currentSlide, prevSlide)
+    })
+
+    // when I click, move slides to the right
+    nextButton.addEventListener('click', e => {
+        const currentSlide = track.querySelector('.current-slide');
+        const nextSlide = currentSlide.nextElementSibling;
+
+        moveToSlide(track, currentSlide, nextSlide);
+        console.log(e.target)
+    })
